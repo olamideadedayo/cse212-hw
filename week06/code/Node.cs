694dbfffb017ab11f98e435b5d80eb0bc613d767
olamideadedayo <olamideadedayo001@gmail.com>
2025-12-05 12:59:07 -0800

    week 6

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -34,12 +34,30 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+         if (value == Data)
+        return true;
+
+    if (value < Data)
+    {
+        if (Left is null)
+            return false;
+        return Left.Contains(value);
+    }
+    else // value > Data
+    {
+        if (Right is null)
+            return false;
+        return Right.Contains(value);
+    }
+    
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+       int leftHeight = (Left == null) ? 0 : Left.GetHeight();
+    int rightHeight = (Right == null) ? 0 : Right.GetHeight();
+
+    return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
--- week06/code/Trees.cs
+++ week06/code/Trees.cs
@@ -46,8 +46,22 @@ public static class Trees
     /// <param name="first">the first index in the sortedNumbers to insert</param>
     /// <param name="last">the last index in the sortedNumbers to insert</param>
     /// <param name="bst">the BinarySearchTree in which to insert the values</param>
-    private static void InsertMiddle(int[] sortedNumbers, int first, int last, BinarySearchTree bst)
-    {
-        // TODO Start Problem 5
-    }
+private static void InsertMiddle(int[] sortedNumbers, int first, int last, BinarySearchTree bst)
+{
+    // Base case: no elements left in this range
+    if (first > last)
+        return;
+
+    // Find the middle index
+    int mid = (first + last) / 2;
+
+    // Insert the middle value into the BST
+    bst.Insert(sortedNumbers[mid]);
+
+    // Recursively insert the left half
+    InsertMiddle(sortedNumbers, first, mid - 1, bst);
+
+    // Recursively insert the right half
+    InsertMiddle(sortedNumbers, mid + 1, last, bst);
+}
 }
